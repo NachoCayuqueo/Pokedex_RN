1a8a70b4395198e8712a7577c26f4e4b67749f87
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-01-10 14:14:41 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {Navigator} from './src/navigator/Navigator';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Navigator />
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Image, Text} from 'react-native';
+import {globalStyles} from '../theme/appTheme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {usePokemonPaginated} from '../hooks/usePokemonPaginated';
+
+export const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
+  usePokemonPaginated();
+
+  return (
+    <>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={globalStyles.pokebolaBG}
+      />
+      <Text
+        style={{
+          ...globalStyles.title,
+          ...globalStyles.globalMargin,
+          top: top + 20,
+        }}>
+        Pokedex
+      </Text>
+    </>
+  );
+};
