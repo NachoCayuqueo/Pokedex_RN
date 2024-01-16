7058526546130a4f0f5c02e624dc2c0ab42a1a21
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2024-01-16 10:34:15 -0300

    tabs navigator

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {Navigator} from './src/navigator/Navigator';
+//import {Navigator} from './src/navigator/Navigator';
 import {NavigationContainer} from '@react-navigation/native';
+import {Tabs} from './src/navigator/Tabs';
 
 const App = () => {
   return (
     <>
       <NavigationContainer>
-        <Navigator />
+        {/* <Navigator /> */}
+        <Tabs />
       </NavigationContainer>
     </>
   );
